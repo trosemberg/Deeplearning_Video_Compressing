ab1f3cfd5ee04837614db657a0d8ed803f61714a
trosemberg <tomasroro@hotmail.com>
2019-09-19 22:36:27 -0300

    modelos salvos + adaptacao do dataloader para todas as entradas

--- myclasses.py
+++ myclasses.py
@@ -3,10 +3,11 @@ from tensorflow import keras
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self,df,Map,batch_size=20,dim=(1,4097),norm=True,n_classes=2, shuffle=True,cte_height = 1080 ,cte_width = 1920):
+    def __init__(self,df,Map,batch_size=20,dim=64,norm=True,n_classes=2, shuffle=True,cte_height = 1080 ,cte_width = 1920):
         self.batch_size = batch_size
         self.df = df
-        self.dim = dim
+        self.dim = (1,(dim*dim)+1)
+        self.size = dim
         self.Map = Map
         self.cte_height = cte_height
         self.cte_width = cte_width
@@ -50,12 +51,12 @@ class DataGenerator(keras.utils.Sequence):
             end_Frame = (int(self.df.loc[ID]['Frame']))*(self.cte_height*self.cte_width + self.cte_height*self.cte_width/2)+self.cte_height*self.cte_width
             begin_Frame = int(begin_Frame)
             end_Frame = int(end_Frame)
-            Input = (file[begin_Frame:end_Frame].reshape([self.cte_height,self.cte_width]))[self.df.loc[ID]['Height']:self.df.loc[ID]['Height']+64,self.df.loc[ID]['Width']:self.df.loc[ID]['Width']+64]
+            Input = (file[begin_Frame:end_Frame].reshape([self.cte_height,self.cte_width]))[self.df.loc[ID]['Height']:self.df.loc[ID]['Height']+self.size,self.df.loc[ID]['Width']:self.df.loc[ID]['Width']+self.size]
             if self.norm:
                 Input = Input/255.0
-                X[i,] = np.append(Input.reshape(1,64*64)[0],self.df.loc[ID]["QP"]/37.0)
+                X[i,] = np.append(Input.reshape(1,self.size*self.size)[0],self.df.loc[ID]["QP"]/37.0)
             else:
-                X[i,] = np.append(Input.reshape(1,64*64)[0],self.df.loc[ID]["QP"])
+                X[i,] = np.append(Input.reshape(1,self.size*self.size)[0],self.df.loc[ID]["QP"])
             # Store class
             y[i,] = self.df.loc[ID]['Split']
 
